746e89691142f950615fd7459a910b6e30681cbd
Your Name <you@example.com>
2023-01-15 19:20:09 +0100

    initial commit

---
+++ PDB/chimera.cmd
@@ -0,0 +1,13 @@
+open axis.bild
+open kk.pdb
+sym #1 group i,2n3r
+combine #1,2
+write #3 i2n3r.pdb
+represent sphere
+turn x -45
+turn y -45
+background solid white
+copy file i2n3r.png width 100 height 100 units millimeters dpi 100
+
+
+# turn  z  90 center 0,0,0
---
+++ README.md
@@ -0,0 +1,4 @@
+# testDataSym
+Test data for testing symmetries
+
+Symmetry definitions are available at URL https://scipion-em.github.io/docs/release-3.0.0/docs/developer/symmetries
