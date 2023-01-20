2cb1cd0de74b42089c31fcbf3733515f50c50aa8
Your Name <you@example.com>
2023-01-20 16:34:17 +0100

    to home

--- Scripts/kk.py
+++ Scripts/kk.py
@@ -5,6 +5,16 @@ import pwem.constants as emcts
 import pwem.convert as emconv
 
 #matrices = emconv.getSymmetryMatrices(emcts.SYM_CYCLIC, n=7)
-# vectorsEdge, vectorsPlane = emconv.getUnitCell(sym=emcts.SYM_CYCLIC, n=7, center=(0, 0, 0), offset=None)
-# vectorsEdge, vectorsPlane = emconv.getUnitCell(sym=emcts.SYM_DIHEDRAL, n=7, center=(0, 0, 0), offset=None)
-vectorsEdge, vectorsPlane = emconv.getUnitCell(sym=emcts.SYM_TETRAHEDRAL_Z3, n=None, center=(0, 0, 0), offset=None)
+# vectorsEdge, vectorsPlane = emconv.getUnitCell(sym=emcts.SYM_CYCLIC, 
+ #n=7, 
+ #center=(0, 0, 0), 
+ #offset=None)
+#vectorsEdge, vectorsPlane = emconv.getUnitCell(
+# sym=emcts.SYM_DIHEDRAL, 
+# n=7, 
+# center=(0, 0, 0), 
+# offset=None)
+vectorsEdge, vectorsPlane = emconv.getUnitCell(sym=emcts.SYM_TETRAHEDRAL_Z3, 
+ n=None, 
+ center=(0, 0, 0), 
+ offset=None)
