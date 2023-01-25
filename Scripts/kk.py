9253c44f75c31918e93e5d4dfa2a6eebd65e87b7
Your Name <you@example.com>
2023-01-25 19:42:26 +0100

    kk.py

--- Scripts/kk.py
+++ Scripts/kk.py
@@ -4,7 +4,10 @@ import pyworkflow.tests as pwtests
 import pwem.constants as emcts
 import pwem.convert as emconv
 
-#matrices = emconv.getSymmetryMatrices(emcts.SYM_CYCLIC, n=7)
+sym = emcts.SYM_TETRAHEDRAL_222
+#sym = emcts.SYM_TETRAHEDRAL_Z3
+#sym = emcts.SYM_TETRAHEDRAL_Z3R
+matrices = emconv.getSymmetryMatrices(sym)
 # vectorsEdge, vectorsPlane = emconv.getUnitCell(sym=emcts.SYM_CYCLIC, 
  #n=7, 
  #center=(0, 0, 0), 
@@ -14,7 +17,7 @@ import pwem.convert as emconv
 # n=7, 
 # center=(0, 0, 0), 
 # offset=None)
-vectorsEdge, vectorsPlane = emconv.getUnitCell(sym=emcts.SYM_TETRAHEDRAL_Z3, 
+vectorsEdge, vectorsPlane = emconv.getUnitCell(sym=sym, 
  n=None, 
  center=(0, 0, 0), 
  offset=None)
